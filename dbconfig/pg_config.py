e922b1a19b0bd2e1514b35288e30d4c38252c10f
Jean Carlos Rivera Hernandez <jean.rivera4@upr.edu>
2021-04-25 14:07:52 -0400

    pgconfig update

--- dbconfig/pg_config.py
+++ dbconfig/pg_config.py
@@ -1,1 +1,1 @@
-pg_config = {'user':'fkbaipfytvhpid','password':'8507c41bdfae4a530701e149a3ee8ab684296c6482d4ead1304133bada2e4834','dbname':'d99dv2t020arv9','dbport':5432}
+pg_config = {'host':'ec2-34-225-167-77.compute-1.amazonaws.com','user':'fkbaipfytvhpid','password':'8507c41bdfae4a530701e149a3ee8ab684296c6482d4ead1304133bada2e4834','dbname':'d99dv2t020arv9','dbport':5432}
